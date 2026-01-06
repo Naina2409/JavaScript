7c34669af003a94f15d87e95e87e2f0228a72fa7
Naina2409 <nainaagarwal2409@gmail.com>
2026-01-06 15:49:44 +0000

    DataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountID =14453
+let accountEmail ="naina@gmail.com"
+var accountPass="12345"
+accountCity="Lucknow"//var can also be declared like this only
+let accountState;//udefined in output
+
+// accountID=2 not allowed
+accountEmail="sc@gmail.com"
+accountPass="2121"
+accountCity="delhi"
+console.log(accountID);
+console.table([accountEmail,accountID,accountPass,accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) we are using node js not browser
+
+let name="naina"
+let age=19
+let isLoggedIn=false
+
+console.log(typeof age);
+console.log(typeof null); //object
+console.log(typeof undefined);//undefined
