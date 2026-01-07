159b59cf36e7ca7c30d5715fa5f2ac9a7ff30272
Naina2409 <nainaagarwal2409@gmail.com>
2026-01-07 16:29:54 +0000

    operations and Containers

---
+++ 01_basics/03_containersOperataion.js
@@ -0,0 +1,78 @@
+// let score ="33abc"-> NaN value as o/p after conversion
+//let score =null //0 value as o/p after conversion
+//let score=undefined//NaN
+let score=true
+
+// console.log(typeof score);
+//or
+// console.log(typeof (score));
+
+let valueInNumber= Number(score)//we can write Number, String..
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let isLoggedIn= 1 -> true
+// let isLoggedIn=""->false
+let isLoggedIn="naina"//->true
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn);
+// console.log( booleanIsLoggedIn);
+
+let someNum=33
+
+let StringNUm= String(someNum)
+// console.log(StringNUm);
+// console.log(typeof StringNUm);
+
+//***** Operations ***** 
+
+let value=3
+let negVal=-value
+// console.log(negVal);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);//power
+// console.log(2/2);
+// console.log(2%2);
+
+let str1="naina"
+let str2=" naina"
+
+let str3=str1+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2");//normal as string
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log((3+4)*5%3);
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1=num2=num3= 2 + 2//readability is not good
+
+let gameCnt=100
+// ++gameCnt;
+gameCnt++;
+console.log(gameCnt);
+console.log(gameCnt++);
+
+
+
+
+
+
+
+
+
+
+
